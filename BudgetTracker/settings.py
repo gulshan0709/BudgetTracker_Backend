c609a789262fe990632a29984a1b6991b18da6af
Gulshan Kumar <gulshan.kumar@realcooderz.in>
2025-05-06 01:45:02 +0530

    code

--- BudgetTracker/settings.py
+++ BudgetTracker/settings.py
@@ -108,14 +108,21 @@ WSGI_APPLICATION = 'BudgetTracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Budget-Tracker',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Budget-Tracker',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
